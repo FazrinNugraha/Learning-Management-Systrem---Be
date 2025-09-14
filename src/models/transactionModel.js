b979bc2f0fd37bb8ca1453a5033637135e6775d2
FazrinNugraha <nugrahafadzrin@gmail.com>
2025-09-14 16:21:26 +0700

    api sign up 1

---
+++ src/models/transactionModel.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo } from "mongoose";
+
+
+const transactionModel = new mongoose.Schema({
+    user : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+    status : {
+        type: String,
+        enum: ['pending', 'succses','failed'],
+        default: 'pending'
+    }
+},{
+    timestamps: true
+})
+
+export default mongoose('transaction', transactionModel)
