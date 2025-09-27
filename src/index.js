c0fd1a6bc9193e8bd247d23594ddd153fa0940e5
FazrinNugraha <nugrahafadzrin@gmail.com>
2025-09-27 08:59:40 +0700

    fix:index js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 import express from "express"
 import dotenv from 'dotenv'
-import bodyParser from "body-parser"
 import cors from 'cors'
 import globalRoutes from "./routes/globalRoutes.js"
 import authRoutes from "./routes/authRoutes.js"
 import connectDB from "./utils/databases.js"
+import paymentRoutes from "./routes/paymentRoutes.js"
 
 const app = express()
-
 dotenv.config()
-
 connectDB()
 
 const port = 3000
+
 app.use(cors())
-app.use(bodyParser.json())
+
+// Parsing body JSON dan urlencoded
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.json({text:'Hello World '})
+  res.json({ text: 'Hello World' })
 })
 
 app.use('/api', globalRoutes)
 app.use('/api', authRoutes)
-
+app.use('/api', paymentRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
