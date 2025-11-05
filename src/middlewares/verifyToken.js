eed13d5ef1263f8752114ae7ec39c69c729d1110
FazrinNugraha <nugrahafadzrin@gmail.com>
2025-11-05 16:57:07 +0700

    add(feat):verify token user

---
+++ src/middlewares/verifyToken.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken"
+import userModel from "../models/userModel.js"
+
+export const verifyToken = async (req, res, next) => {
+    const secretKey = process.env.SECRET_KEY_JWT ?? ""
+
+    if (req?.headers?.authorization?.split(" ")[0] === "JWT") {
+        const decoded = jwt.verify(
+            req?.headers?.authorization?.split(" ")[1],
+            secretKey
+        )
+
+        const user = await userModel.finsdById(
+            decoded.data.id,
+            "_id name email role"
+        )
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Unauthorized access",
+            })
+        }
+        req.user = {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+        }
+        next()
+    } else {
+        return json.status(401).json({
+            message: "Unauthorized access - Token missing",
+        })
+    }
+}
