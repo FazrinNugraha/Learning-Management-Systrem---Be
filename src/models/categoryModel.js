a04e54424a1de0610c568f3dbc8ea8bc9b7259e9
FazrinNugraha <nugrahafadzrin@gmail.com>
2025-11-05 17:03:09 +0700

    add(feat): schema for categoryModel

---
+++ src/models/categoryModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        
+    },
+    courses:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+    }]
+},{
+    timestamps: true,
+})
+
+export default mongoose.model("Category", categorySchema);  
