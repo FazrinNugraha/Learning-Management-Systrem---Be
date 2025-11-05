eecd113f84195c2168bf7134dc2b497cda783672
FazrinNugraha <nugrahafadzrin@gmail.com>
2025-11-05 17:03:22 +0700

    add(feat): schema for courseDetailModel

---
+++ src/models/courseDetailModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const courseDetailModel = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        enum: ['video', 'text'],
+        deafult: 'video'
+    },
+    videoId: String,
+    textId: String,
+
+    course:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+    },
+    
+},{timestamps: true,})
+
+export default mongoose.model("CourseDetail", courseDetailModel);   
