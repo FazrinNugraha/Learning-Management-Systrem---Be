1a1ad5df1b1be1438669dbc9740a0d1caa6c68b0
FazrinNugraha <nugrahafadzrin@gmail.com>
2025-11-07 20:56:59 +0700

    add(feat):postCourse for create course

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -1,4 +1,7 @@
 import courseModel from "../models/courseModel.js";
+import fs from 'fs';
+import { mutateCourseSchema } from "../utils/schema.js";
+import userModel from "../models/userModel.js";
 
 export const getCourses = async (req, res) => {
     try {
@@ -24,6 +27,71 @@ export const getCourses = async (req, res) => {
     } catch (error) {
         return res.status(400).json ({
             message: "Internal Error Server"
+        })
+    }
+}
+
+export const postCourse = async (req, res) => {
+    try {
+        const body = req.body
+
+        const parse = mutateCourseSchema.parse(body)
+
+        if (!parse.success) {
+            const errorMessages = parse.error.errors.map(e => e.message)
+
+            if(req?.file?.path && fs.existsSync(req?.file?.path)) {
+                fs.unlinkSync (req?.file?.path)
+
+        }
+
+        return res.status(400).json ({
+            message: "Validation Error",
+            errors: errorMessages
+        })
+
+    }
+    const category = await courseModel.findById(parse.data.categoryId)
+
+    if (!category) {
+        return res.status(400).json ({
+            message: "Category not found"
+        })
+    }
+
+    const course = new courseModel ({
+        name: parse.data.name,
+        category: category._id,
+        desctiption: parse.data.description,
+        tagline: parse.data.tagline,
+        thumbnail: req.file?.filname,
+        manager: user._id,
+    })
+
+    await course.save()
+
+    await categoryModel.findByIdAndUpdate(category._id, {
+        $push: {
+            courses: course._id
+        },
+    }, {new: true})
+
+     await userModel.findByIdAndUpdate(category._id, {
+        $push: {
+            courses: course._id
+        },
+    }, {new: true})
+
+    return res.json ({
+        message: "Course created successfully",
+        data: course
+    })
+
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json ({
+            message: "Internal Server Error"
         })
     }
 }
