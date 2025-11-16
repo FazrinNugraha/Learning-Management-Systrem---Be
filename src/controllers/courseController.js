b4da8f6f8bb09448b3c76fb1e3505910c1fa8cea
FazrinNugraha <nugrahafadzrin@gmail.com>
2025-11-16 23:53:33 +0700

    add(feat):thumbnailUrl and totalStudents with map.item

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -21,9 +21,19 @@ export const getCourses = async (req, res) => {
             select: 'name '
         })
 
+        const imageUrl = process.env.APP_URL + '/uploads/courses/'
+
+        const response = courses.map((item) => {
+          return {
+            ...item.toObject(),
+            thumbnailUrl: imageUrl + item.thumbnail,
+            totalStudents: item.students.length
+          }
+        })
+
         return res.json({
             message: "Get courses succses",
-            data: courses
+            data: response
         })
     } catch (error) {
         return res.status(400).json ({
@@ -86,6 +96,64 @@ export const postCourse = async (req, res) => {
     return res.status(201).json({
       message: "Course created successfully",
       data: course,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+export const updateCourse = async (req, res) => {
+  try {
+    const body = req.body;
+
+    const courseId = req.params.id
+
+    
+
+    // Validasi input pakai Zod
+    const parse = mutateCourseSchema.safeParse(body);
+    if (!parse.success) {
+      const errorMessages = parse.error.issues.map(e => e.message);
+
+      // Hapus file kalau ada, tapi validasi gagal
+      if (req?.file?.path && fs.existsSync(req.file.path)) {
+        fs.unlinkSync(req.file.path);
+      }
+
+      return res.status(400).json({
+        message: "Validation Error",
+        data: null,
+        errors: errorMessages,
+      });
+    }
+
+    // ✅ Cari kategori di collection Category
+    const category = await categoryModel.findById(parse.data.categoryId)
+    const oldCourse = await courseModel.findById(courseId)
+
+    if (!category) {
+      return res.status(400).json({
+        message: "Category not found",
+      });
+    }
+
+    await courseModel.findByIdAndUpdate(
+      courseId,{
+        name: parse.data.name,
+        category: category._id,
+        description: parse.data.description,
+        tagline: parse.data.tagline,
+        thumbnail: req?.file ? req.file?.filename : oldCourse.thumbnail,
+        manager: req.user._id,
+      }
+    )
+
+    return res.status(201).json({
+      message: "Updated courses successfully",
     });
   } catch (error) {
     console.error(error);
