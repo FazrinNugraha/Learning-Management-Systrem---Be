9dc5f33ce79e50621e092497365dada165e94fb5
FazrinNugraha <nugrahafadzrin@gmail.com>
2025-12-30 21:39:02 +0700

    add(feat):api get categories

--- src/routes/coursesRoutes.js
+++ src/routes/coursesRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getCourses, updateCourse } from '../controllers/courseController.js';
+import { getCategories, getCourses, updateCourse } from '../controllers/courseController.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
 import multer from 'multer';
 import { fileStorageCourses, fileFilter } from '../utils/multer.js';
@@ -14,9 +14,11 @@ const upload = multer ({
 })
 
 courseRoutes.get('/courses', verifyToken, getCourses)
+courseRoutes.get('/categories', verifyToken, getCategories)
 courseRoutes.post('/courses', verifyToken, upload.single('thumbnail'), postCourse)
 courseRoutes.put('/courses/:id', verifyToken, upload.single('thumbnail'), updateCourse)
 courseRoutes.delete('/courses/:id', verifyToken, deleteCourse)
+
 
 
 
