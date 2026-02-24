0104628ba6262322cc7b9d67ae9f07bd533730a0
FazrinNugraha <nugrahafadzrin@gmail.com>
2026-02-24 22:16:19 +0700

    feat(add): api update and delete for students

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -2,16 +2,29 @@ import userModel from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import { mutateCourseSchema, mutateStudentSchema } from "../utils/schema.js";
 import fs from "fs";
+import path from "path";
 
 export const getStudent = async (req, res) => {
   try {
-    const student = await userModel.find({
-      role: "student",
-      manager: req.user._id,
+    const student = await userModel
+      .find({
+        role: "student",
+        manager: req.user._id,
+      })
+      .select("name courses photo");
+
+    const photoUrl = process.env.APP_URL + "/uploads/students/";
+
+    const response = student.map((item) => {
+      return {
+        ...toObject(),
+        photo_url: photoUrl + ClipboardItem.photo,
+      };
     });
+
     return res.status(200).json({
       message: "Get students successfully",
-      data: student,
+      data: response,
     });
   } catch (error) {
     return res.status(500).json({
@@ -55,6 +68,99 @@ export const postStudent = async (req, res) => {
     return res.status(201).json({
       message: "Student created successfully",
       data: student,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+export const updateStudent = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+
+    const parse = mutateStudentSchema
+      .partial({
+        password: true,
+      })
+      .safeParse(body);
+
+    if (!parse.success) {
+      const errorMessages = parse.error.issues.map((e) => e.message);
+
+      if (req?.file?.path && fs.existsSync(req.file.path)) {
+        fs.unlinkSync(req.file.path);
+      }
+
+      return res.status(400).json({
+        message: "Validation Error",
+        data: null,
+        errors: errorMessages,
+      });
+    }
+
+    const student = await userModel.findById(id);
+
+    const hashedPassword = parse.data.password
+      ? await bcrypt.hashSync(parse.data.password, 12)
+      : student.password;
+
+    await userModel.findByIdAndUpdate(id, {
+      name: parse.data.name,
+      email: parse.data.email,
+      password: hashedPassword,
+      photo: req.file ? req.file.filename : student.photo,
+    });
+
+    await student.save();
+    return res.status(201).json({
+      message: "Update created successfully",
+      data: student,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
+};
+
+export const deleteStudent = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const student = await userModel.findById(id);
+
+    await userModel.findOneAndUpdate(
+      {
+        students: id,
+      },
+      {
+        $pull: {
+          students: id,
+        },
+      },
+    );
+
+    const dirname = path.resolve();
+    const filePath = path.join(
+      dirname,
+      "public/uploads/students",
+      student.photo,
+    );
+
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+    }
+
+    await userModel.findByIdAndDelete(id);
+
+    return res.status(200).json({
+      message: "Delete student successfully",
+      data: null,
     });
   } catch (error) {
     return res.status(500).json({
--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getStudent } from '../controllers/studentController.js';
+import { deleteStudent, getStudent, updateStudent } from '../controllers/studentController.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
 import multer from 'multer';
 import { fileFilter, fileStorage } from '../utils/multer.js';
@@ -14,6 +14,10 @@ const upload = multer({
 
 studentRoutes.get('/students', verifyToken, getStudent);
 studentRoutes.post('/students', verifyToken, upload.single('avatar'), postStudent);
+studentRoutes.put('/students/:id', verifyToken, upload.single('avatar'), updateStudent);
+studentRoutes.delete('/students/:id', verifyToken, deleteStudent);
+
+
 
 
 export default studentRoutes;
